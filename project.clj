1b188b6c89980c0e604da991d4570abd0c1dcd6a
i-blis <i-blis@yandex.ru>
2014-07-15 03:15:46 +0200

    license wtf

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jdbc-fu "0.1.0"
+(defproject jdbc-fu "0.1.1"
   :description "A collection of utility functions on top of java.jdbc"
   :url "https://github.com/i-blis/jdbc-fu"
   :license {:name "WTFPL"
